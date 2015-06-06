8e4824c39f13a65731f9b1d0bee12bc4be497e91
Justin <booljayj@gmail.com>
2015-06-05 16:01:35 -0800

    Removed "Current" properties

--- Assets/Celestial Mechanics/Scripts/CelestialOrbitDecay.cs
+++ Assets/Celestial Mechanics/Scripts/CelestialOrbitDecay.cs
@@ -14,18 +14,12 @@ namespace CelestialMechanics {
 		public float argumentOriginal {get; private set;}
 		public double periodOriginal {get; private set;}
 
-		public double semiMajorAxisCurrent {get; private set;}
-		public float argumentCurrent {get; private set;}
-		public double periodCurrent {get; private set;}
-
 		void Awake() {
 			orbit = GetComponent<CelestialOrbit>();
 
 			semiMajorAxisOriginal = orbit.semiMajorAxis;
 			argumentOriginal = orbit.argument;
 			periodOriginal = orbit.period;
-
-			ResetSimulation();
 		}
 
 		void Update() {
@@ -36,40 +30,30 @@ namespace CelestialMechanics {
 		public void StopSimulation() {orbit.simulate = false;}
 
 		public void ResetSimulation() {
-			semiMajorAxisCurrent = semiMajorAxisOriginal;
 			orbit.semiMajorAxis = semiMajorAxisOriginal;
-
-			argumentCurrent = argumentOriginal;
 			orbit.argument = argumentOriginal;
-
-			periodCurrent = periodOriginal;
 			orbit.period = periodOriginal;
 		}
 
 		public void UpdateSimulation() {
 			if (orbitDecay != 0) {
-				semiMajorAxisCurrent += orbitDecay * Time.deltaTime * orbit.timeScale;
-				periodCurrent = Math.Sqrt( (periodOriginal*periodOriginal) *
-				                           (semiMajorAxisCurrent*semiMajorAxisCurrent*semiMajorAxisCurrent) /
-				                           (semiMajorAxisOriginal*semiMajorAxisOriginal*semiMajorAxisOriginal) );
+				orbit.semiMajorAxis += orbitDecay * Time.deltaTime * orbit.timeScale;
 
-				if (semiMajorAxisCurrent <= 0) {
+				if (orbit.semiMajorAxis <= 0) {
 					//behaviour beyond this point is undefined, best not upset the Kraken
 					ResetSimulation();
 					StopSimulation();
 					orbit.StopSimulation();
 
 				} else {
-					orbit.semiMajorAxis = semiMajorAxisCurrent;
-					orbit.period = periodCurrent;
+					orbit.period = Math.Sqrt( (periodOriginal*periodOriginal) *
+					                          (orbit.semiMajorAxis*orbit.semiMajorAxis*orbit.semiMajorAxis) /
+					                          (semiMajorAxisOriginal*semiMajorAxisOriginal*semiMajorAxisOriginal) );
 				}
 			}
 
 			if (apsidalPrecession != 0) {
-				argumentCurrent = Kepler.WrapAngle(argumentCurrent - 
-				                                   apsidalPrecession * Time.deltaTime * (float)orbit.timeScale, -180, 180);
-
-				orbit.argument = argumentCurrent;
+				orbit.argument = Kepler.WrapAngle(orbit.argument - apsidalPrecession * Time.deltaTime * (float)orbit.timeScale, -180, 180);
 			}
 		}
 	}
