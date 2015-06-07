9bdbd3031c957031233e21fe22777a23f6b1ebd6
Justin <booljayj@gmail.com>
2015-06-07 15:09:31 -0800

    Fixed indenting and defaults
    Attempt to acquire orbit from attached gameObject if not specified.

--- Assets/Celestial Mechanics/Scripts/CelestialOrbitPath.cs
+++ Assets/Celestial Mechanics/Scripts/CelestialOrbitPath.cs
@@ -3,47 +3,51 @@ using System;
 using System.Collections;
 
 namespace CelestialMechanics {
-[RequireComponent(typeof(LineRenderer)), ExecuteInEditMode]
-public class CelestialOrbitPath : MonoBehaviour {
-	const double Deg2Rad = Math.PI/180.0;
-	
-	public CelestialOrbit orbit;
-	[Range(3,200)] public int segments = 50;
+	[RequireComponent(typeof(LineRenderer)), ExecuteInEditMode]
+	public class CelestialOrbitPath : MonoBehaviour {
+		const double Deg2Rad = Math.PI/180.0;
+		
+		public CelestialOrbit orbit;
+		[Range(3,200)] public int segments = 50;
 
-	LineRenderer line;
+		LineRenderer line;
 
-	void Awake() {
-		line = GetComponent<LineRenderer>();
-	}
+		void Awake() {
+			line = GetComponent<LineRenderer>();
 
-	void OnEnable() {
-		if (orbit == null) {
-			line.SetVertexCount(0);
+			if (orbit == null) {
+				orbit = GetComponent<CelestialOrbit>();
+			}
+		}
 
-		} else {
-			double step, lower, E, v, r;
-			line.SetVertexCount(segments+1);
+		void OnEnable() {
+			if (orbit == null) {
+				line.SetVertexCount(0);
 
-			step = (orbit.limits.y - orbit.limits.x)*Deg2Rad/segments;
-			lower = orbit.limits.x*Deg2Rad;
+			} else {
+				double step, lower, E, v, r;
+				line.SetVertexCount(segments+1);
 
-			for (int i = 0; i < segments+1; i++) {
-				E = Kepler.ComputeEccentricAnomaly(lower+step*i, orbit.eccentricity);
-				v = Kepler.ComputeTrueAnomaly(E, orbit.eccentricity);
-				r = Kepler.ComputeRadius(orbit.semiLatusRectum, orbit.eccentricity, v);
-				if (orbit.transform.parent) {
-					line.SetPosition(i, orbit.transform.parent.TransformPoint(Kepler.ComputePosition(r, v)));
-				} else {
-					line.SetPosition(i, Kepler.ComputePosition(r, v));
+				step = (orbit.limits.y - orbit.limits.x)*Deg2Rad/segments;
+				lower = orbit.limits.x*Deg2Rad;
+
+				for (int i = 0; i < segments+1; i++) {
+					E = Kepler.ComputeEccentricAnomaly(lower+step*i, orbit.eccentricity);
+					v = Kepler.ComputeTrueAnomaly(E, orbit.eccentricity);
+					r = Kepler.ComputeRadius(orbit.semiLatusRectum, orbit.eccentricity, v);
+					if (orbit.transform.parent) {
+						line.SetPosition(i, orbit.transform.parent.TransformPoint(Kepler.ComputePosition(r, v)));
+					} else {
+						line.SetPosition(i, Kepler.ComputePosition(r, v));
+					}
 				}
 			}
 		}
-	}
 
-	#if UNITY_EDITOR
-	void Update() {
-		OnEnable();
+		#if UNITY_EDITOR
+		void Update() {
+			OnEnable();
+		}
+		#endif
 	}
-	#endif
-}
 }
