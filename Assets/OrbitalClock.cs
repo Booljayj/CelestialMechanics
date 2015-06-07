98cd6f0f5bcf6e2fe88e1ae010f7484a5a067bd3
Justin <booljayj@gmail.com>
2015-06-07 15:47:08 -0800

    Added Orbital Clock example
    And associated script

---
+++ Assets/OrbitalClock.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace CelestialMechanics {
+	public class OrbitalClock : MonoBehaviour {
+		public CelestialOrbit seconds;
+		public CelestialOrbit minutes;
+		public CelestialOrbit hours;
+		public CelestialOrbit days;
+
+		void Start () {
+			System.DateTime time = System.DateTime.Now;
+			seconds.meanAnomaly = (float)time.Millisecond/1000f*360f;
+			minutes.meanAnomaly = (float)time.Second/60f*360f;
+			hours.meanAnomaly = (float)time.Minute/60f*360f;
+			days.meanAnomaly = (float)time.Hour/24f*360f;
+
+			seconds.ResetSimulation();
+			minutes.ResetSimulation();
+			hours.ResetSimulation();
+			days.ResetSimulation();
+		}
+	}
+}
