4c147e5bbef478da88854d65de1e9c12b7129a78
Justin <booljayj@gmail.com>
2015-08-07 14:09:47 -0800

    Add Screenshot component

---
+++ Assets/ScreenShot.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ScreenShot : MonoBehaviour {
+	void Update() {
+		if (Input.GetMouseButtonDown(0)) {
+			Application.CaptureScreenshot(Application.dataPath + "/Screenshot.png", 2);
+			Debug.Log("Screenshot taken");
+		}
+	}
+}
