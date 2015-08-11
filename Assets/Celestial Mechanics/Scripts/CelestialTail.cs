87e3d1f9aeb839c5da0b3f972a2d38b5520fb452
Justin <booljayj@gmail.com>
2015-08-11 15:02:37 -0800

    Add CelestialTail script

---
+++ Assets/Celestial Mechanics/Scripts/CelestialTail.cs
@@ -0,0 +1,8 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CelestialTail : MonoBehaviour {
+	public void SetAnomaly(double anomaly) {
+		transform.localRotation = Quaternion.Euler(0f, -(float)(anomaly)*Mathf.Rad2Deg, 0f);
+	}
+}
