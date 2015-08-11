7aee8328358be7983e56396add08fb8ac3dc4fac
Justin <booljayj@gmail.com>
2015-08-11 10:18:21 -0800

    Add Name Parameter To Screenshot Component

--- Assets/Logo/ScreenShot.cs
+++ Assets/Logo/ScreenShot.cs
@@ -2,9 +2,11 @@
 using System.Collections;
 
 public class ScreenShot : MonoBehaviour {
+	public string name;
+
 	void Update() {
 		if (Input.GetMouseButtonDown(0)) {
-			Application.CaptureScreenshot(Application.dataPath + "/Screenshot.png", 2);
+			Application.CaptureScreenshot(Application.dataPath + "/"+name+".png", 2);
 			Debug.Log("Screenshot taken");
 		}
 	}
